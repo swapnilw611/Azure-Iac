8d3bf6a57c59a4f57c214e725e7bed64e77c1abc
swapnilw611 <swapnilw611@gmail.com>
2026-02-06 21:01:50 +0530

    change machine type

--- main.tf
+++ main.tf
@@ -124,7 +124,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm1"
   resource_group_name = local.resource_group_name
   location            = local.location
-  size                = "Standard_B1ms"
+  size                = "Standard_B2ms"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
 
