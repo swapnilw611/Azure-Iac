0e69d21c470906efa743691640e66c95b6d5ad1b
swapnilw611 <swapnilw611@gmail.com>
2026-02-06 19:41:16 +0530

    change location

--- main.tf
+++ main.tf
@@ -21,128 +21,128 @@ resource "azurerm_resource_group" "rg" {
 
 }
 
-# Virtual Network
-resource "azurerm_virtual_network" "vnet1" {
-  name                = local.virtual_network.name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = local.virtual_network.address_space
+# # Virtual Network
+# resource "azurerm_virtual_network" "vnet1" {
+#   name                = local.virtual_network.name
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   address_space       = local.virtual_network.address_space
 
-  depends_on = [ azurerm_resource_group.rg ]
+#   depends_on = [ azurerm_resource_group.rg ]
 
-  tags = {
-    environment = "test"
-  }
-}
+#   tags = {
+#     environment = "test"
+#   }
+# }
 
-# Subnet
-resource "azurerm_subnet" "subnet-a" {
-  name                 = local.subnets[0].name
-  resource_group_name  = local.resource_group_name
-  virtual_network_name = local.virtual_network.name
-  address_prefixes     = local.subnets[0].address_prefixes
+# # Subnet
+# resource "azurerm_subnet" "subnet-a" {
+#   name                 = local.subnets[0].name
+#   resource_group_name  = local.resource_group_name
+#   virtual_network_name = local.virtual_network.name
+#   address_prefixes     = local.subnets[0].address_prefixes
   
-  depends_on = [ azurerm_virtual_network.vnet1 ]
-}
+#   depends_on = [ azurerm_virtual_network.vnet1 ]
+# }
 
-# Network Interface
-resource "azurerm_network_interface" "nic1" {
-  name                = "nic"
-  location            = local.location
-  resource_group_name = local.resource_group_name
+# # Network Interface
+# resource "azurerm_network_interface" "nic1" {
+#   name                = "nic"
+#   location            = local.location
+#   resource_group_name = local.resource_group_name
 
-  ip_configuration {
-    name                          = "internal1"
-    subnet_id                     = azurerm_subnet.subnet-a.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.public-ip1.id
-  }
-  depends_on = [ azurerm_subnet.subnet-a ]
-}
+#   ip_configuration {
+#     name                          = "internal1"
+#     subnet_id                     = azurerm_subnet.subnet-a.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id = azurerm_public_ip.public-ip1.id
+#   }
+#   depends_on = [ azurerm_subnet.subnet-a ]
+# }
 
-# Public IP
-resource "azurerm_public_ip" "public-ip1" {
-  name                = "TestPublicIp1"
-  resource_group_name = local.resource_group_name
-  location            = local.location
-  allocation_method   = "Static"
-  sku                 = "Standard"
+# # Public IP
+# resource "azurerm_public_ip" "public-ip1" {
+#   name                = "TestPublicIp1"
+#   resource_group_name = local.resource_group_name
+#   location            = local.location
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
 
-  tags = {
-    environment = "test"
-  }
-  depends_on = [ azurerm_resource_group.rg ]
-}
+#   tags = {
+#     environment = "test"
+#   }
+#   depends_on = [ azurerm_resource_group.rg ]
+# }
 
-# Network Security Group
-resource "azurerm_network_security_group" "nsg" {
-  name                = "TestSecurityGroup1"
-  location            = local.location
-  resource_group_name = local.resource_group_name
+# # Network Security Group
+# resource "azurerm_network_security_group" "nsg" {
+#   name                = "TestSecurityGroup1"
+#   location            = local.location
+#   resource_group_name = local.resource_group_name
 
-  security_rule {
-    name                       = "Allow-HTTP"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                      = "Allow"
-    protocol                    = "Tcp"
-    source_port_range           = "*"
-    destination_port_range      = "80"
-    source_address_prefix       = "*"
-    destination_address_prefix  = "*"
-  }
+#   security_rule {
+#     name                       = "Allow-HTTP"
+#     priority                   = 100
+#     direction                  = "Inbound"
+#     access                      = "Allow"
+#     protocol                    = "Tcp"
+#     source_port_range           = "*"
+#     destination_port_range      = "80"
+#     source_address_prefix       = "*"
+#     destination_address_prefix  = "*"
+#   }
 
-  security_rule {
-    name                       = "Allow-SSH"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                      = "Allow"
-    protocol                    = "Tcp"
-    source_port_range           = "*"
-    destination_port_range      = "22"
-    source_address_prefix       = "*"
-    destination_address_prefix  = "*"
-  }
+#   security_rule {
+#     name                       = "Allow-SSH"
+#     priority                   = 110
+#     direction                  = "Inbound"
+#     access                      = "Allow"
+#     protocol                    = "Tcp"
+#     source_port_range           = "*"
+#     destination_port_range      = "22"
+#     source_address_prefix       = "*"
+#     destination_address_prefix  = "*"
+#   }
 
-  tags = {
-    environment = "test"
-  }
+#   tags = {
+#     environment = "test"
+#   }
 
-  depends_on = [ azurerm_resource_group.rg ]
-}
+#   depends_on = [ azurerm_resource_group.rg ]
+# }
 
-resource "azurerm_subnet_network_security_group_association" "nsg-association1" {
-  subnet_id                 = azurerm_subnet.subnet-a.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+# resource "azurerm_subnet_network_security_group_association" "nsg-association1" {
+#   subnet_id                 = azurerm_subnet.subnet-a.id
+#   network_security_group_id = azurerm_network_security_group.nsg.id
 
-  depends_on = [ azurerm_network_security_group.nsg,azurerm_subnet.subnet-a ]
-}
+#   depends_on = [ azurerm_network_security_group.nsg,azurerm_subnet.subnet-a ]
+# }
 
-# Linux Virtual Machine
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm1"
-  resource_group_name = local.resource_group_name
-  location            = local.location
-  size                = "Standard_B1ms"
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
+# # Linux Virtual Machine
+# resource "azurerm_linux_virtual_machine" "vm" {
+#   name                = "vm1"
+#   resource_group_name = local.resource_group_name
+#   location            = local.location
+#   size                = "Standard_B1ms"
+#   admin_username      = var.admin_username
+#   admin_password      = var.admin_password
 
-  disable_password_authentication = false
+#   disable_password_authentication = false
 
-  network_interface_ids = [
-    azurerm_network_interface.nic1.id
-  ]
+#   network_interface_ids = [
+#     azurerm_network_interface.nic1.id
+#   ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  depends_on = [ azurerm_resource_group.rg,azurerm_network_interface.nic1 ]
-}
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts"
+#     version   = "latest"
+#   }
+#   depends_on = [ azurerm_resource_group.rg,azurerm_network_interface.nic1 ]
+# }
